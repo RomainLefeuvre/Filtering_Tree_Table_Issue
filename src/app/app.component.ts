8db6fa84806322848eb7a1bbd7bbe8a4fec9c9b7
Lefeuvre Romain <romain.lefeuvre.1@etudiant.univ-rennes1.fr>
2020-09-14 17:44:07 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NodeService} from './nodeservice';
+import {TreeNode} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'tree-table-issue-v3';
+    files: TreeNode[];
+
+    cols: any[];
+
+    constructor(private nodeService: NodeService) { }
+
+    ngOnInit() {
+        this.nodeService.getFilesystem().then(files => this.files = files);
+
+        this.cols = [
+            { field: 'FirstName', header: 'First Name' },
+            { field: 'LastName', header: 'LastName' },
+        ];
+    }
 }
